0e92bc44a3f5cb47f21aa87ecdeb8e006d17338c
shaw <shawbahrampour@gmail.com>
2023-05-02 19:19:03 +0330

    added physicial, psychological and socail health pages ref #7

---
+++ pages/psychological.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="d-flex flex-column align-center h-full text-center" style="background-color: #f3911f">
+    <h1 class="bel mt-16 f-80 white--text text--darken-3 font-weight-regular">Psychological Health</h1>
+    <p class="bel mt-6 f-20 white--text text--darken-3">
+      Enjoyment of social interaction, sharing experiences, sense of belonging to a group, promoting participation  
+    </p>
+    <v-img class="mt-16" contain src="/images/video_content/psychological.png" />
+    <div class="ma-16 h-full text-center">
+      <h4 class="bel mt-16 f-40 white--text text--darken-3 font-weight-regular">
+        32 Million people in need of 2 Billion doses of iron supplement; less than 800 Million doses is consumed
+      </h4>
+      <p class="bel mt-6 f-20 white--text text--darken-3">
+        WHO advises that all women in their reproductive ages, whether having Iron deficiency or not, are required to
+        consume 60 mg of iron supplements per week. This amount should be higher in pregnant women and any deficiencies
+        will not only affect themselves, but it also affects their fetus. In addition, Iranian men and menopaused women
+        have a 2 prevalence of Anemia and are also require to manage the disease accordingly (3).
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: this.$t('pageTitles.psychological-health')
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
