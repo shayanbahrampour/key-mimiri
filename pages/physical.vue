909f0b54d3f08cd22b866e8f5a5af3e8221b973f
shaw <shawbahrampour@gmail.com>
2023-05-10 10:39:30 +0330

    added mobile responsive to the social, psycological and physical pages ref #7

--- pages/physical.vue
+++ pages/physical.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="d-flex flex-column align-center h-full text-center" style="background-color: #00a59b">
-    <h1 class="bel mt-16 f-80 white--text text--darken-3 font-weight-regular">Physical Health</h1>
+  <div class="d-flex flex-column align-center text-center" style="background-color: #00a59b">
+    <h1 :class="['bel mt-16 white--text text--darken-3 font-weight-regular', isMobile ? 'f-40' : 'f-80']">
+      Physical Health
+    </h1>
     <p class="bel mt-6 f-20 white--text text--darken-3">
       Enjoyment of social interaction, sharing experiences, sense of belonging to a group, promoting participation
     </p>
     <v-img class="mt-16" contain src="/images/video_content/physical.png" />
-    <div class="ma-16 h-full text-center">
+    <div :class="[isMobile ? 'text-start px-6' : 'text-center ma-16']">
       <h4 class="bel mt-16 f-40 white--text text--darken-3 font-weight-regular">
         32 Million people in need of 2 Billion doses of iron supplement; less than 800 Million doses is consumed
       </h4>
@@ -15,6 +17,19 @@
         will not only affect themselves, but it also affects their fetus. In addition, Iranian men and menopaused women
         have a 2 prevalence of Anemia and are also require to manage the disease accordingly (3).
       </p>
+    </div>
+    <div v-if="isMobile" class="w-full d-flex flex-column align-center px-6 mb-16">
+      <h1 class="bel f-40 mt-16 white--text text--darken-3 font-weight-regularl">Tell us your exprience</h1>
+      <v-btn
+        class="rounded-xl d-flex justify-center white--text f-20 mt-4"
+        outlined
+        color="white"
+        height="50"
+        elevation="0"
+        min-width="100%"
+      >
+        Click here
+      </v-btn>
     </div>
   </div>
 </template>
--- pages/psychological.vue
+++ pages/psychological.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="d-flex flex-column align-center h-full text-center" style="background-color: #f3911f">
-    <h1 class="bel mt-16 f-80 white--text text--darken-3 font-weight-regular">Psychological Health</h1>
+  <div class="d-flex flex-column align-center text-center" style="background-color: #f3911f">
+    <h1 :class="['bel mt-16 white--text text--darken-3 font-weight-regular', isMobile ? 'f-40' : 'f-80']">
+      Psychological Health
+    </h1>
     <p class="bel mt-6 f-20 white--text text--darken-3">
       Enjoyment of social interaction, sharing experiences, sense of belonging to a group, promoting participation
     </p>
     <v-img class="mt-16" contain src="/images/video_content/psychological.png" />
-    <div class="ma-16 h-full text-center">
+    <div :class="[isMobile ? 'text-start px-6 h-full psy-screen d-flex flex-column' : 'text-center ma-16']">
       <h4 class="bel mt-16 f-40 white--text text--darken-3 font-weight-regular">
         32 Million people in need of 2 Billion doses of iron supplement; less than 800 Million doses is consumed
       </h4>
@@ -15,6 +17,11 @@
         will not only affect themselves, but it also affects their fetus. In addition, Iranian men and menopaused women
         have a 2 prevalence of Anemia and are also require to manage the disease accordingly (3).
       </p>
+      <v-spacer></v-spacer>
+      <div v-if="isMobile" class="d-flex justify-space-between">
+        <div class="right-circle mb-16"></div>
+        <div class="left-circle mb-16"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -29,4 +36,24 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.psy-screen {
+  height: 100vh !important;
+}
+
+.right-circle {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background-color: #00a59b;
+  margin-left: -70px;
+}
+
+.left-circle {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background-color: #a01e64;
+  margin-right: -70px;
+}
+</style>
