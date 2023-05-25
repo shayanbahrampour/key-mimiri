d56bf09d8b6efa1aed7a1bd6e4084d8f62520eae
shaw <shawbahrampour@gmail.com>
2023-05-25 05:09:07 +0330

    fixed a bug ref #19

--- pages/storytellers.vue
+++ pages/storytellers.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="d-flex flex-column slategrey">
     <div
-      :class="[
-        'd-flex px-16 mt-16 align-center justify-space-between',
-        isMobile ? 'flex-column align-center' : undefined
-      ]"
+      :class="['d-flex align-center justify-space-between', isMobile ? 'flex-column align-center mt-6' : 'px-16 mt-16']"
     >
       <div :class="['d-flex', isMobile ? 'flex-column align-center' : undefined]">
         <v-img
@@ -25,7 +22,7 @@
           <p
             :class="[
               'f-20 white--text mb-0 text--darken-3',
-              isMobile ? 'text-center mt-4 font-weight-light' : undefined
+              isMobile ? 'text-center mt-2 font-weight-light' : undefined
             ]"
           >
             Project Manager at Cobel® Group
@@ -44,16 +41,41 @@
         </v-btn>
       </div>
     </div>
-    <p :class="['mt-16 f-20 mx-16 white--text text--darken-3 font-weight-light', isMobile ? 'text-center' : undefined]">
+    <p
+      v-if="isMobile"
+      :class="['f-20 mx-16 white--text text--darken-3 font-weight-light', isMobile ? 'text-center mt-8 mb-0' : 'mt-16']"
+      style="line-height: 30px"
+    >
+      Who advises that all women in their reproductive ages, whether having Iron deficiency or not, are required to
+      consume 60 mg of iron
+    </p>
+    <p
+      v-else
+      :class="['f-20 mx-16 white--text text--darken-3 font-weight-light', isMobile ? 'text-center mt-8 mb-0' : 'mt-16']"
+      style="line-height: 30px"
+    >
       WHO advises that all women in their reproductive ages, whether having Iron deficiency or not, are required to
       consume 60 mg of iron supplements per week. This amount should be higher in pregnant women and any deficiencies
       will not only affect themselves, but it also affects their fetus. In addition, Iranian men and menopaused women
       have a 2 prevalence of Anemia and are also require to manage the disease accordingly (3). Studies conducted by
       Cobel Group medico-marketing teams indicated
     </p>
-    <v-img class="mt-16" contain src="/images/video_content/physical.png" />
-    <div class="h-full white px-16">
-      <h4 class="bel mt-16 f-40 text-center text--darken-3 font-weight-regular" style="color: #00a59b">
+    <v-btn
+      v-if="isMobile"
+      class="bel pa-0 ma-0 mt-2 justify-center f-20 font-weight-bold"
+      color="white"
+      depressed
+      rounded
+      style="text-transform: unset !important"
+      text
+      ><p>see more</p></v-btn
+    >
+    <v-img :class="[isMobile ? 'mt-4' : 'mt-16']" contain src="/images/video_content/physical.png" />
+    <div :class="['h-full white', isMobile ? 'px-4' : 'px-16']">
+      <h4
+        :class="['bel f-40 text-center text--darken-3 font-weight-regular', isMobile ? 'mt-6' : 'mt-16']"
+        style="color: #00a59b"
+      >
         32 Million people in need of 2 Billion doses of iron supplement; less than 800 Million doses is consumed
       </h4>
       <v-divider v-if="!isMobile" class="mt-16"></v-divider>
@@ -79,13 +101,13 @@
           <div class="d-flex flex-column justify-center ml-4">
             <p
               :class="[
-                'bel f-26 black--text mb-0 text--darken-3 font-weight-bold',
-                isMobile ? 'text-center' : undefined
+                'bel black--text mb-0 text--darken-3',
+                isMobile ? 'text-center mt-4 f-32 font-weight-regular' : 'f-26 font-weight-regular'
               ]"
             >
               Bahador Nayebi
             </p>
-            <p :class="['bel f-20 mb-0 text--darken-3', isMobile ? 'mt-4' : undefined]" style="color: #939393">
+            <p :class="['bel  mb-0 text--darken-3', isMobile ? 'mt-2 f-24' : '-20']" style="color: #939393">
               Project Manager at Cobel® Group
             </p>
             <v-divider v-if="isMobile" class="my-4" style="background-color: #00a59b"></v-divider>
@@ -114,13 +136,13 @@
           <div class="d-flex flex-column justify-center ml-4">
             <p
               :class="[
-                'bel f-26 black--text mb-0 text--darken-3 font-weight-bold',
-                isMobile ? 'text-center' : undefined
+                'bel black--text mb-0 text--darken-3',
+                isMobile ? 'text-center mt-4 f-32 font-weight-regular' : 'f-26 font-weight-regular'
               ]"
             >
               Mohamad javid
             </p>
-            <p :class="['bel f-20 mb-0 text--darken-3', isMobile ? 'mt-4' : undefined]" style="color: #939393">
+            <p :class="['bel  mb-0 text--darken-3', isMobile ? 'mt-2 f-24' : '-20']" style="color: #939393">
               Project Manager at Cobel® Group
             </p>
             <v-divider v-if="isMobile" class="my-4" style="background-color: #00a59b"></v-divider>
