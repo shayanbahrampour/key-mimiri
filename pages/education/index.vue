4508ce3d27f9999dc19f25d425373a7f3344f338
shaw <shawbahrampour@gmail.com>
2023-05-26 21:41:00 +0330

    fixed a style ref #19

--- pages/education/index.vue
+++ pages/education/index.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="d-flex flex-column">
     <v-img
-      :class="['d-flex align-end', !isMobile ? 'px-10' : 'px-6']"
+      :class="['d-flex align-end', !isMobile ? 'px-16' : 'px-5']"
       :height="isMobile ? '30vh' : '65vh'"
       src="/images/education/header.png"
     >
       <h4 v-if="!isMobile" class="bel white--text f-70 font-weight-regular" style="margin-bottom: 140px">EDUCATION</h4>
       <h4 v-else class="bel white--text f-36 font-weight-regular mb-4">Education</h4>
     </v-img>
-    <div v-if="!isMobile" class="d-flex flex-column align-start w-full white">
+    <div v-if="!isMobile" class="d-flex flex-column align-start w-full white pr-4">
       <v-card
         :width="isMobile ? '90%' : undefined"
-        class="rounded-r-xl white--text rounded-l-0 d-flex py-6 px-10 flex-column mr-14 align-center justify-center"
+        class="rounded-r-xl white--text rounded-l-0 d-flex py-6 px-10 flex-column mr-16 align-center justify-center"
         color="slategrey"
         elevation="0"
         height="240"
         style="margin-top: -120px"
       >
-        <p class="f-30 mb-0">
+        <p class="f-30 mb-0 mx-6">
           Empowers to achieve, Inspires to create the greatest Cobel Group promise is to empower, inspire and trigger
           ideas that make a significant impact in how we deliver solutions. Therefore, we invest on education; not only
           to our personnel, but to our stakeholders. Our education material are available to all.
