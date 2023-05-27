c6033e3d19c5b9d61a5f6554fe1c8c4fc5100831
shaw <shawbahrampour@gmail.com>
2023-05-27 15:10:57 +0330

    fixed a style ref #19

--- components/shared/ShareOptions.vue
+++ components/shared/ShareOptions.vue
@@ -2,14 +2,35 @@
   <div :class="[isMobile ? 'mt-2' : 'd-flex align-center']">
     <span :class="['f-16 slategrey--text', isMobile ? undefined : 'mr-4']">share it:</span>
     <div :class="[isMobile ? 'mt-2' : undefined]">
-      <v-btn class="rounded" color="slategrey" height="50" min-width="50" outlined>
-        <v-img height="20" max-width="20" src="/images/icons/linkedin-primary.png" />
+      <v-btn
+        color="slategrey"
+        height="50"
+        min-width="50"
+        width="50"
+        outlined
+        style="border-radius: 35%; border: 3px solid slategrey"
+      >
+        <v-img height="18" max-width="18" src="/images/icons/linkedin-primary.png" />
       </v-btn>
-      <v-btn class="ml-4 rounded" color="slategrey" height="50" min-width="50" outlined>
-        <v-img height="20" max-width="20" src="/images/icons/whatsapp-primary.png" />
+      <v-btn
+        class="ml-4"
+        height="50"
+        min-width="50"
+        width="50"
+        outlined
+        style="border-radius: 35%; border: 3px solid slategrey"
+      >
+        <v-img height="18" max-width="18" src="/images/icons/whatsapp-primary.png" />
       </v-btn>
-      <v-btn class="ml-4 rounded" color="slategrey" height="50" min-width="50" outlined>
-        <v-img height="20" max-width="20" src="/images/icons/telegram-primary.png" />
+      <v-btn
+        class="ml-4"
+        height="50"
+        min-width="50"
+        width="50"
+        outlined
+        style="border-radius: 35%; border: 3px solid slategrey"
+      >
+        <v-img height="18" max-width="18" src="/images/icons/telegram-primary.png" />
       </v-btn>
     </div>
   </div>
