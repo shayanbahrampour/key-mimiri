23ee77267a599f4e49f41e85ed03d69ccccdeda6
shaw <shawbahrampour@gmail.com>
2023-05-31 19:14:13 +0330

    refactored some codes ref #19

---
+++ components/news/NewsCarousel.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="position-relative">
+    <v-carousel v-model="model" :show-arrows="false" class="position-relative z-0" cycle height="auto" hide-delimiters>
+      <v-carousel-item v-for="(item, index) in items" :key="index">
+        <v-img :height="isMobile ? '30vh' : 'calc(100vh - 120px)'" :src="item.src" class="position-relative z-0" />
+      </v-carousel-item>
+    </v-carousel>
+    <div
+      v-if="!isMobile"
+      :class="['position-absolute start-8 end-0 bottom-0 mx-auto z-0 px-16 mr-10']"
+      :style="`max-width: ${globalMaxWidth}px; bottom: ${isMobile ? 260 : 40}px`"
+    >
+      <v-icon
+        v-for="(item, index) in items"
+        :key="index"
+        :class="`m${isRTL ? 'r' : 'l'}-1`"
+        color="white"
+        size="25"
+        @click="model = index"
+      >
+        {{ index === model ? 'mdi-radiobox-marked' : 'mdi-radiobox-blank' }}
+      </v-icon>
+    </div>
+
+    <div class="position-relative z-1">
+      <div :class="['mx-auto', !isMobile && `p${isRTL ? 'l' : 'r'}-4`]" :style="`max-width: ${globalMaxWidth}px`">
+        <v-sheet
+          v-if="!isMobile"
+          class="ml-6 rounded-t-xl py-6 px-12 slategrey"
+          height="480"
+          width="480"
+          style="margin-top: -480px"
+        >
+          <h4 class="bel mt-16 f-50 text--darken-3 white--text font-weight-regular">13th Episode of Co-Talk Event</h4>
+          <p class="mt-6 f-20 white--text text--darken-3 mb-0 font-weight-light">
+            Iron deficiency, leading to Anemia, has negative health effects on all individuals, specially women...
+          </p>
+          <v-btn
+            class="bel pa-0 ma-0 mt-2 justify-start f-18 font-weight-bold"
+            color="white"
+            depressed
+            rounded
+            style="text-transform: unset !important"
+            text
+            ><p>{{ $t('shared.see_more') }}</p></v-btn
+          >
+
+          <v-btn
+            class="rounded-xl d-flex mt-10 justify-center f-14 font-weight-regular"
+            color="white"
+            height="40"
+            min-width="60%"
+            outlined
+          >
+            register now...
+          </v-btn>
+        </v-sheet>
+        <v-sheet
+          v-else
+          class="ma-0 rounded-t-xl py-10 px-8 d-flex flex-column align-start slategrey"
+          style="position: relative; z-index: 2; margin-top: -72px !important"
+        >
+          <h4 class="bel mt-6 f-40 text--darken-3 white--text font-weight-regular text-start" style="max-width: 300px">
+            13th Episode of Co-Talk Event
+          </h4>
+          <p class="mt-6 f-20 white--text text--darken-3 font-weight-light mb-0">
+            Iron deficiency, leading to Anemia, has negative health effects on all individuals, especially women...
+          </p>
+          <v-btn
+            class="pa-0 ma-0 justify-start f-18 font-weight-bold"
+            color="white"
+            depressed
+            rounded
+            style="text-transform: unset !important"
+            text
+            ><p>{{ $t('shared.see_more') }}</p></v-btn
+          >
+          <v-btn
+            class="rounded-xl d-flex justify-center f-20 my-6 font-weight-light"
+            color="white"
+            height="40"
+            min-width="100%"
+            outlined
+          >
+            register now
+          </v-btn>
+        </v-sheet>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      model: 0
+    };
+  },
+  computed: {
+    activeSlide() {
+      return this.items[this.model];
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.carousel-sheet {
+  transition: all ease-in 0.4s;
+
+  &:after {
+    z-index: 0;
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    height: 100%;
+    width: 50vw;
+    background-color: var(--v-slategrey-base);
+  }
+}
+
+.v-application--is-rtl {
+  .carousel-sheet:after {
+    right: 0;
+    left: auto !important;
+  }
+}
+</style>
--- pages/news.vue
+++ pages/news.vue
@@ -1,71 +1,15 @@
 <template>
   <div class="d-flex flex-column pb-16">
-    <v-img v-if="!isMobile" :class="['d-flex align-end px-10']" height="85vh" src="/images/news/slider.png">
-      <v-sheet class="ml-6 rounded-t-xl py-6 px-12 slategrey" height="480" width="480">
-        <h4 class="bel mt-16 f-50 text--darken-3 white--text font-weight-regular">13th Episode of Co-Talk Event</h4>
-        <p class="mt-6 f-20 white--text text--darken-3 mb-0 font-weight-light">
-          Iron deficiency, leading to Anemia, has negative health effects on all individuals, specially women...
-        </p>
-        <v-btn
-          class="bel pa-0 ma-0 mt-2 justify-start f-18 font-weight-bold"
-          color="white"
-          depressed
-          rounded
-          style="text-transform: unset !important"
-          text
-          ><p>{{ $t('shared.see_more') }}</p></v-btn
-        >
-
-        <v-btn
-          class="rounded-xl d-flex mt-10 justify-center f-14 font-weight-regular"
-          color="white"
-          height="40"
-          min-width="60%"
-          outlined
-        >
-          register now...
-        </v-btn>
-      </v-sheet>
-    </v-img>
-    <div v-else>
-      <v-img
-        :class="['d-flex align-end px-0']"
-        height="40vh"
-        src="/images/news/slider.png"
-        style="position: relative; z-index: 1; margin-bottom: -70px"
-      >
-      </v-img>
-
-      <v-sheet
-        class="ma-0 rounded-t-xl py-10 px-8 d-flex flex-column align-start slategrey"
-        style="position: relative; z-index: 2"
-      >
-        <h4 class="bel mt-6 f-40 text--darken-3 white--text font-weight-regular text-start" style="max-width: 300px">
-          13th Episode of Co-Talk Event
-        </h4>
-        <p class="mt-6 f-20 white--text text--darken-3 font-weight-light mb-0">
-          Iron deficiency, leading to Anemia, has negative health effects on all individuals, especially women...
-        </p>
-        <v-btn
-          class="pa-0 ma-0 justify-start f-18 font-weight-bold"
-          color="white"
-          depressed
-          rounded
-          style="text-transform: unset !important"
-          text
-          ><p>{{ $t('shared.see_more') }}</p></v-btn
-        >
-        <v-btn
-          class="rounded-xl d-flex justify-center f-20 my-6 font-weight-light"
-          color="white"
-          height="40"
-          min-width="100%"
-          outlined
-        >
-          register now
-        </v-btn>
-      </v-sheet>
-    </div>
+    <NewsCarousel
+      :items="[
+        {
+          src: '/images/news/slider.png'
+        },
+        {
+          src: '/images/news/slider.png'
+        }
+      ]"
+    />
 
     <v-sheet v-if="!isMobile" :max-width="globalMaxWidth" class="mx-auto px-10 my-16">
       <NewsCategory v-if="!isMobile" :tabs="tabs" class="mb-16 mx-6" title="More important" />
@@ -78,9 +22,10 @@
 <script>
 import NewsCategory from '~/components/news/NewsCategory';
 import NewsCard from '~/components/news/NewsCard';
+import NewsCarousel from '~/components/news/NewsCarousel';
 
 export default {
-  components: { NewsCategory, NewsCard },
+  components: { NewsCategory, NewsCard, NewsCarousel },
   head() {
     return {
       title: this.$t('pageTitles.press')
