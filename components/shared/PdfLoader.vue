cbaf122c53b00958b0368d835f71407d768617b0
shaw <shawbahrampour@gmail.com>
2023-06-07 22:08:54 +0330

    added some features

--- components/shared/PdfLoader.vue
+++ components/shared/PdfLoader.vue
@@ -1,50 +1,63 @@
 <template>
   <div class="position-relative z-0">
-    <vue-pdf
-      :page="page"
-      :src="src"
-      :style="`font-family: inherit; height: ${flag.showContent ? 'auto' : '400px'}`"
-      class="w-full position-relative z-0 ltr white h-auto"
-      @error="onError"
-      @num-pages="pageCount = $event"
-    />
+    <div ref="pdfContainer">
+      <vue-pdf
+        :page="page"
+        :src="src"
+        :style="`font-family: inherit; height: ${flag.showContent ? 'auto' : '400px'}`"
+        class="w-full position-relative z-0 ltr white h-auto"
+        @error="onError"
+        @num-pages="pageCount = $event"
+      />
 
-    <v-btn
-      v-if="page < pageCount"
-      :class="['shadow white top-0 bottom-0 my-auto z-1 end-0', isMobile ? 'mx-3' : 'mx-6']"
-      :height="isMobile ? 30 : 40"
-      :width="isMobile ? 30 : 40"
-      absolute
-      fab
-      icon
-      @click="next"
-    >
-      <v-icon color="#66869A" size="28">mdi-chevron-{{ isRTL ? 'left' : 'right' }}</v-icon>
-    </v-btn>
-    <v-btn
-      v-if="page > 1"
-      :class="['shadow white top-0 bottom-0 my-auto z-1 mxart-0', isMobile ? 'mx-3' : 'mx-6']"
-      :height="isMobile ? 30 : 40"
-      :width="isMobile ? 30 : 40"
-      absolute
-      fab
-      icon
-      @click="prev"
-    >
-      <v-icon color="#66869A" size="28">mdi-chevron-{{ isRTL ? 'right' : 'left' }}</v-icon>
-    </v-btn>
-    <v-btn
-      v-if="isMobile"
-      absolute
-      class="shadow white bottom-0 end-0 z-1 ma-3"
-      fab
-      height="30"
-      icon
-      width="30"
-      @click="prev"
-    >
-      {{ pageCount }}
-    </v-btn>
+      <v-btn
+        v-if="page < pageCount"
+        :class="['shadow white top-0 bottom-0 my-auto z-1 end-0', isMobile ? 'mx-3' : 'mx-6']"
+        :height="isMobile ? 30 : 40"
+        :width="isMobile ? 30 : 40"
+        absolute
+        fab
+        icon
+        @click="next"
+      >
+        <v-icon color="#66869A" size="28">mdi-chevron-{{ isRTL ? 'left' : 'right' }}</v-icon>
+      </v-btn>
+      <v-btn
+        v-if="page > 1"
+        :class="['shadow white top-0 bottom-0 my-auto z-1 mxart-0', isMobile ? 'mx-3' : 'mx-6']"
+        :height="isMobile ? 30 : 40"
+        :width="isMobile ? 30 : 40"
+        absolute
+        fab
+        icon
+        @click="prev"
+      >
+        <v-icon color="#66869A" size="28">mdi-chevron-{{ isRTL ? 'right' : 'left' }}</v-icon>
+      </v-btn>
+      <v-btn
+        v-if="isMobile"
+        absolute
+        class="shadow white bottom-0 end-0 z-1 ma-3"
+        fab
+        height="30"
+        icon
+        width="30"
+        @click="prev"
+      >
+        {{ pageCount }}
+      </v-btn>
+      <v-btn
+        absolute
+        class="shadow white bottom-0 start-0 z-1 ma-3"
+        fab
+        height="30"
+        icon
+        width="30"
+        @click="toggleFullscreen()"
+      >
+        <v-icon color="#66869A" size="16">mdi-square-outline</v-icon>
+      </v-btn>
+    </div>
   </div>
 </template>
 
@@ -74,6 +87,19 @@ export default {
     },
     prev() {
       this.page--;
+    },
+    toggleFullscreen() {
+      const container = this.$refs.pdfContainer;
+
+      if (container.requestFullscreen) {
+        container.requestFullscreen();
+      } else if (container.mozRequestFullScreen) {
+        container.mozRequestFullScreen();
+      } else if (container.webkitRequestFullscreen) {
+        container.webkitRequestFullscreen();
+      } else if (container.msRequestFullscreen) {
+        container.msRequestFullscreen();
+      }
     }
   }
 };
