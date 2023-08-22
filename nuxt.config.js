0055f6e656df41fe685ee35cfbe50669b98ad409
shaw <shawbahrampour@gmail.com>
2023-08-22 17:42:29 +0330

    added gtag scripts to header

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,6 +63,20 @@ export default {
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/images/icons/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/images/icons/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/images/icons/favicon-16x16.png' }
+    ],
+    script: [
+      {
+        async: true,
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-ESS8P8H60C'
+      },
+      {
+        innerHTML: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ESS8P8H60C');
+        `
+      }
     ]
   },
   loading: {
