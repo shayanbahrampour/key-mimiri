37c9acd44acc1fd021a335b52afffe9196fcfe9f
shaw <shawbahrampour@gmail.com>
2023-10-12 01:16:42 +0330

    added spotify component

--- components/home/HomeSlider.vue
+++ components/home/HomeSlider.vue
@@ -6,28 +6,51 @@
   >
     <v-sheet
       :class="['position-relative z-1 mx-auto d-flex align-center', isMobile ? 'px-8' : 'px-16']"
-      :style="isMobile ? '' : 'top:-30px;'"
       color="transparent"
       height="100%"
       min-height="500"
     >
-      <h1
-        :class="[
-          'white--text text--darken-2 font-weight-regular',
-          isMobile ? 'f-40' : 'f-50',
-          isRTL && isMobile && 'text-center'
-        ]"
-        style="font-family: IBM Plex Mono; line-height: 1.4"
-      >
-        Shaw Bahrampour:<br />
-        Riding the Data Wave with Retro Vibes - A Front-End Developer's Paradise
-      </h1>
+      <div class="d-flex flex-column align-end" :style="!isMobile && `height: 90%`">
+        <h1
+          :class="[
+            'white--text text--darken-2 font-weight-regular',
+            isMobile ? 'f-40' : 'f-50',
+            isRTL && isMobile && 'text-center'
+          ]"
+          style="font-family: IBM Plex Mono; line-height: 1.4"
+        >
+          Shaw Bahrampour:<br />
+          Riding the Data Wave with Retro Vibes - A Front-End Developer's Paradise
+        </h1>
+        <v-sheet
+          v-if="!isMobile"
+          class="rounded d-flex flex-column align-center"
+          color="transparent"
+          width="100%"
+          height="100%"
+          style="border: solid 2px grey !important"
+        >
+          <iframe
+            title="Spotify Embed: Recommendation Playlist "
+            src="https://open.spotify.com/embed/playlist/1UNUQqWRrpTgigGsXA7Fiy?utm_source=generator&theme=0"
+            width="100%"
+            height="180"
+            frameBorder="0"
+            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+            loading="lazy"
+            class="rounded"
+          />
+          <span class="white--text f-24" style="font-family: IBM Plex Mono; line-height: 1.4"
+            >In Praise of the Palm Tree🌴</span
+          >
+        </v-sheet>
+      </div>
     </v-sheet>
     <v-sheet
       v-if="!isMobile"
       class="mr-16 rounded overflow-hidden"
-      height="600"
-      min-width="500"
+      height="90%"
+      min-width="35%"
       color="transparent"
       style="border: solid 2px grey !important"
     >
@@ -35,7 +58,7 @@
         src="https://my.spline.design/darkspideycopy-064a63ac1ff82ba5aeebc45c26b69ec8/"
         frameborder="0"
         width="100%"
-        height="110%"
+        height="115%"
         style="border-radius: 14px"
       ></iframe>
     </v-sheet>
